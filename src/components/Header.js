b59cb9e2fbc3fe386a5dae772580512e9b126d9c
NextSap <nextsap@outlook.com>
2023-01-26 11:51:41 +0100

    Header + starting Home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="flex items-center justify-between h-[15vh] w-full bg-[#F5F5F5]">
+            <div className="flex space-x-3 ml-16">
+                <h1>Logo</h1>
+                <h1>Nom de l'Ei</h1>
+            </div>
+            <div>
+                <ul className="flex space-x-10 text-[#919191] mr-5">
+                    <NavLink to={"/"} className={activePage("/")}>
+                        <li>Accueil</li>
+                    </NavLink>
+                    <NavLink to={"/about"} className={activePage("about")}>
+                        <li>A propos</li>
+                    </NavLink>
+                    <NavLink to={"/work"} className={activePage("work")}>
+                        <li>Préstations</li>
+                    </NavLink>
+                    <NavLink to={"/projects"} className={activePage("projects")}>
+                        <li>Projets</li>
+                    </NavLink>
+                    <NavLink to={"/contact"} className={activePage("contact")}>
+                        <li>Contact</li>
+                    </NavLink>
+                </ul>
+            </div>
         </div>
     );
 };
 
+const checkActive = (match) => {
+    if (match === "/")
+        return document.URL.charAt(document.URL.length - 1) === match;
+    return document.URL.split("/")[document.URL.split("/").length - 1] === match;
+};
+const activePage = (match) => {
+    if (checkActive(match))
+        return "text-[#919191]";
+    return "text-[#919191] hover:text-gray-300";
+}
 export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Header from "../components/Header";
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="flex flex-col h-full w-full bg-[#F5F5F5]">
+            <Header />
+            <div
+                className="flex flex-col-reverse md:flex-row bg-[#F5F5F5] h-full md:h-auto w-full overflow-hidden">
+                <div className="flex justify-center items-center h-[50vh] md:h-[90vh] md:w-[50vw] bg-[#F5F5F5]">
+                    <img className="h-full w-full object-cover" src={require("../assets/bardage_background.png")} alt=""/>
+                </div>
+                <div className="flex flex-col h-full justify-center items-center text-[#919191] tracking-widest">
+                    <div className="w-[50%] text-6xl lg:text-8xl mb-5 text-center mb-8 md:mb-26 mt-2 md:mt-0">
+                        <h1>Revêtissez votre maison</h1>
+                        <button className="bg-gray-300 text-xl p-3 rounded-2xl">Obtenir un devis</button>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 };
 
